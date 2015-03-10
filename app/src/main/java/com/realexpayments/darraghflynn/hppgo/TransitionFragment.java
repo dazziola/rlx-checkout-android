6f0a66626e2c08220ed253fc411496278b62ef2f
Darragh Flynn <Darragh.Flynn@realexpayments.com>
2015-03-10 17:13:36 +0000

    Webview code started

--- app/src/main/java/com/realexpayments/darraghflynn/hppgo/TransitionFragment.java
+++ app/src/main/java/com/realexpayments/darraghflynn/hppgo/TransitionFragment.java
@@ -4,9 +4,12 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.transition.Scene;
 import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
 import android.widget.Button;
 
 /**
@@ -20,13 +23,16 @@ public class TransitionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_transition_scene_1, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_transition_scene_1, container, false);
         final Scene scene = Scene.getSceneForLayout(container, R.layout.fragment_transition_scene_2, getActivity());
         Button goButton = (Button)rootView.findViewById(R.id.goButton);
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TransitionManager.go(scene);
+                WebView browser = (WebView)rootView.findViewById(R.id.webview);
+                browser.loadUrl("www.google.com");
+                Log.d("","Here");
             }
         });
         return rootView;
